2615f968800dd917e7aa9a89bb49c725f1a2d951
Benjamin Kim <benkim96@gmail.com>
2020-08-25 21:07:12 -0700

    controller for image CRUD established

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -8,18 +8,31 @@ class ImagesController < ApplicationController
   end
 
   def new
+    @image = Image.new
   end
 
   def edit
+    @image = Image.find(params[:id])
   end
 
   def create
     @image = Image.new(image_params)
-    @image.save
-    redirect_to @image
+
+    if @image.save
+      redirect_to @image
+    else
+      render 'new'
+    end
   end
 
   def update
+    @image = Image.find(params[:id])
+
+    if @image.update(image_params)
+      redirect_to @image
+    else
+      render 'edit'
+    end
   end
 
   def destroy
